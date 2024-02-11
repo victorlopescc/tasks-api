9117d4542fa730b429eb087c26ede07a56c52392
victorlopescc <victorlopes.cc@gmail.com>
2024-02-10 23:31:09 -0300

    feat: Delete

--- src/database.js
+++ src/database.js
@@ -47,7 +47,12 @@ export class Database {
 
     }
 
-    delete() {
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id);
 
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1);
+            this.#persist();
+        }
     }
 }
